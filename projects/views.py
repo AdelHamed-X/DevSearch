99529a1a1222bac988a06068e7a1bcf3c75a25d6
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-14 14:25:25 +0300

    Adding delete operation to opject

--- projects/views.py
+++ projects/views.py
@@ -15,10 +15,10 @@ def projects(request):
 
 
 def project(request, pk):
-    projct = Project.objects.get(id=pk)
+    project = Project.objects.get(id=pk)
 
     context = {
-        'project': projct,
+        'project': project,
     }
     return render(request, 'projects/project.html', context)
 
@@ -32,19 +32,36 @@ def create_project(request):
             form.save()
             return redirect('projects')
 
-    context = {'form': form}
+    context = {
+        'form': form,
+    }
     return render(request, 'projects/project_form.html', context)
 
 
 def update_project(request, pk):
-    projectobj = Project.objects.get(id=pk)
-    form = ProjectForm(instance=projectobj)
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
 
     if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=projectobj)
+        form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
 
-    context = {'form': form}
+    context = {
+        'form': form,
+    }
     return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {
+        'object': project,
+    }
+    return render(request, 'projects/delete.html', context)
